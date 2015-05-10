2d82a0c2d5880ea2b2bf25220220c0f13ab30f39
Sharon237 <sf.engbers@gmail.com>
2015-05-10 14:22:11 +0200

    test 4

--- Persistent/src/nl/saxion/persistent/MainActivity.java
+++ Persistent/src/nl/saxion/persistent/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Toast.makeText(this, "Hallo4", Toast.LENGTH_LONG).show();
+		Toast.makeText(this, "Hallo5", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
